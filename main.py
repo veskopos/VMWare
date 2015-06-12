c5a2be14012227f16ffd07acbce430e43c6b7a1f
veskopos <veskopos@mail.bg>
2015-06-12 11:24:35 +0300

    Adding files

---
+++ main.py
@@ -0,0 +1,26 @@
+from api.requests_api import RequestsApi
+from graph import graph
+import threading
+
+def clean_sector(request, sector, current_graph = False):
+	if not current_graph:
+		current_graph = graph.make_graph(request, sector)
+	print sector
+	while current_graph.edges_size > 0:
+		path = current_graph.search_path()
+		request.send_trajectory(sector, path) 
+		current_graph.update(request)
+	print "Ending thread %d" % sector
+
+threads = []
+
+sectors = range(1, 11)
+request = RequestsApi()
+
+for sector in sectors:
+	t = threading.Thread(target=clean_sector, args=(request, sector,))
+	threads.append(t)
+	t.start()
+
+for thread in threads:
+	thread.join()
